57e91369219de888b145d2c9c2d3ec6fede41984
marc <marcvanhelvoort@gmail.com>
2024-04-04 12:34:22 +0200

    First setup

--- example-domain/target/generated-sources/annotations/nl/spindltree/order/OrderDto.java
+++
@@ -1,9 +0,0 @@
-package nl.spindltree.order;
-
-import lombok.Value;
-@Value
-public class OrderDto  {
-        long id;
-        java.lang.String description;
-        nl.spindltree.order.OrderStatus status;
-}
