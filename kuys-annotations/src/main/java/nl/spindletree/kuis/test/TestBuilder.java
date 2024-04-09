4b75572772f01b217c144be0f0c2e19ccbaee90c
Marc van helvoort <marcvanhelvoort@gmail.com>
2024-04-09 15:42:24 +0200

    Add spring controller to spring-example (#3)
    * Add spring controller to spring-example
    
    * Add spring controller to spring-example
    
    * Add spring controller to spring-example

---
+++ kuys-annotations/src/main/java/nl/spindletree/kuis/test/TestBuilder.java
@@ -0,0 +1,14 @@
+package nl.spindletree.kuis.test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface TestBuilder {
+    Class<?> baseClass();
+
+    String contentFile();
+}
